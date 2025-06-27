7308a525abce4a82847ceac83554dc7e9f987217
Ravishankar Sharma <ravishankarsharma1006@gmail.com>
2025-06-28 00:11:28 +0530

    Initialize frontend project with Vite, React, and Tailwind CSS;  and add backend folder

--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -27,14 +27,14 @@ router.route("/create-task").post(isAuth, taskValidator(), validate, createTask)
 router.route("/update-task/:taskId").put(isAuth, taskValidator(), validate, updateTask);
 router.route("/delete-task/:taskId").delete(isAuth, deleteTask);
 router.route("/status/:taskId").put(isAuth, taskStatus);
-router.route("/project/:projectId").get(isAuth, getTaskByProjectId);
+router.route("/tasks/:projectId").get(isAuth, getTaskByProjectId);
 
 
 // Subtask routes
-router.route("/subtasks").get(isAuth, getAllSubTasks);
-router.route("/subtasks/create").post(isAuth, taskValidator(), validate, createSubTask);
-router.route("/subtasks/update/:id").put(isAuth, taskValidator(), validate, updateSubTask);
-router.route("/subtasks/delete/:id").delete(isAuth, deleteSubTask);
+router.route("/").get(isAuth, getAllSubTasks);
+router.route("/create").post(isAuth, taskValidator(), validate, createSubTask);
+router.route("/update/:id").put(isAuth, taskValidator(), validate, updateSubTask);
+router.route("/delete/:id").delete(isAuth, deleteSubTask);
 
 router.route("/:taskId").get(isAuth, getTaskById);
 
